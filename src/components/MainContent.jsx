4ff58f0975610fc926a8aecdbf5348b551ea6504
Bancy Wacuka <bancywacuka@gmail.com>
2025-07-21 00:39:17 +0300

    Styled components with inline CSS

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f5f5f5', padding: '15px', fontSize: '18px' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
