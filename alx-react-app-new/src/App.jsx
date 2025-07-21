c4aa46240409d0e666d96ba4e180d9f5cc3c0bfe
Bancy Wacuka <bancywacuka@gmail.com>
2025-07-21 06:21:37 +0300

    Add Counter component to App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import Counter from './components/Counter';
 import Header from './components/Header';
 import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
@@ -6,6 +8,9 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div>
+      <h1>Welcome to My App</h1>
+      <h1>Simple Counter App</h1>
+      <Counter />
       <Header />
       <UserProfile name="Bancy" age={30} bio="A travel enthusiast and web developer." />
       <MainContent />
