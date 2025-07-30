9ad769a963851f44a97acdd58dc7fe8c30997fdd
Bancy Wacuka <bancywacuka@gmail.com>
2025-07-31 01:55:12 +0300

    Fix: Update error message to match checker requirement

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -28,7 +28,7 @@ function Search() {
         setDetailedUser(detailed); // Optional for display
       }
     } catch (err) {
-      setError('Looks like we can’t find the user');
+      setError('Looks like we cant find the user');
     } finally {
       setLoading(false);
     }
