20f8ba5eb931e739db50fc382710f4c013fbc69f
Bancy Wacuka <bancywacuka@gmail.com>
2025-07-31 01:31:59 +0300

    Implement advanced GitHub user search and UI enhancements

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -19,7 +19,7 @@ function Search() {
       const data = await fetchUsersAdvanced(username, location, minRepos);
       setUsers(data.items); // GitHub Search API returns an object with `items`
     } catch (err) {
-      setError('Looks like we can’t find the user');
+      setError('Looks like we cant find the user');
     } finally {
       setLoading(false);
     }
