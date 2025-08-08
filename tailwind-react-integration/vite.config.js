10ebf39748dcf304b282988de9d7311d3fa5f936
Bancy Wacuka <bancywacuka@gmail.com>
2025-08-08 17:45:07 +0300

    Fix Tailwind CSS configuration

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vue()],
   css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
+    postcss: './postcss.config.js', // ✅ Make sure this exists
   },
 })
