4bfa367d14ca4d1884a90e29a333b9a5c6442ab3
Bancy Wacuka <bancywacuka@gmail.com>
2025-08-10 20:58:10 +0300

    Implement responsive padding and styles for UserProfile component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,15 @@
-// src/components/UserProfile.jsx
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
-        alt="User avatar"
-        className="rounded-full w-36 h-36 mx-auto"
+        alt="User"
+        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
       />
-      <h1 className="text-xl text-blue-800 my-4">Bancy Wacuka</h1>
-      <p className="text-gray-600 text-base">
-        Developer B and Daughters co. Loves to write code and explore new technologies.
+      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">Bancy Wacuka</h1>
+      <p className="text-gray-600 sm:text-sm md:text-base">
+        Developer at B and daughter Co. Loves to write code and explore new
+        technologies.
       </p>
     </div>
   );
