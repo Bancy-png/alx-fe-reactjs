8bc5106ae516363575c8552195115724201ab372
Bancy Wacuka <bancywacuka@gmail.com>
2025-08-21 14:19:59 +0300

    feat: add Formik form with Yup validation

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,10 +1,12 @@
 import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   return (
     <div>
       <h1>Form Handling React Task</h1>
       <RegistrationForm />
+      <FormikForm />
     </div>
   );
 }
