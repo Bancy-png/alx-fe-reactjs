8d44fa83b82a102962790ddc95ece5b05f08e929
Bancy Wacuka <bancywacuka@gmail.com>
2025-08-23 01:07:45 +0300

    Fix Jest setup and initial Todo App test

--- react-todo/jest.config.js
+++ react-todo/jest.config.js
@@ -1,10 +1,13 @@
+// jest.config.js
 export default {
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
-  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -3,6 +3,7 @@ import TodoList from "./components/TodoList";
 export default function App() {
   return (
     <div style={{ padding: 20, fontFamily: "system-ui, sans-serif" }}>
+      <h1>Todo App</h1>
       <TodoList />
     </div>
   );
